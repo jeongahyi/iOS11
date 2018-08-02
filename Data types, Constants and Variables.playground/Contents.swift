988f5a846853bff914b2fdfacc55e97854bc4211
jeongahyi <jeongahyi@gmail.com>
2018-08-03 00:26:34 +0900

    added Dice, I-Am-Poor

---
+++ Data types, Constants and Variables.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var myAge : Int = 29
+
+myAge = 30
+
+let myName : String = "Jay"
+
+let mySurName : String = "Yi"
+//myName = "J" error
+
+let myAgeInTenYears = myAge + 10
+
+//let myFullName = myName + "Yi"
+let myFullName = "\(myName) \(mySurName)"
+
+let myDetails = "\(myName),\(myAge)"
